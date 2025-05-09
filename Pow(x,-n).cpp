122d9f8c87627c2bcc9dd0a80dd5659f05e6e9c9
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-05-09 16:06:44 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 71.74%] - LeetPush

---
+++ Pow(x,-n).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans =1 ; 
+        long long nn = n ; 
+        if(nn<0){
+            nn = -1*nn ; 
+        }
+        while(nn){
+            if(nn%2==0){
+                x = x*x ; 
+                nn = nn/2 ;
+            }else{
+                ans = ans*x ; 
+                nn= nn-1 ; 
+            }
+        }
+        if(n<0){
+            ans = 1/ans ; 
+            return ans ; 
+        }
+        return ans ; 
+    }
+};
