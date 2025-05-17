91f5f0a8286e2af16e7b13cee10527ebf75ebc64
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-05-17 12:13:04 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 11.80%] - LeetPush

--- Sort-Colors.cpp
+++ Sort-Colors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
+        int l = 0 ; 
+        int m = 0 ; 
+        int n = nums.size() ; 
+        int h = n-1 ; 
+        while(m<=h){
+            if(nums[m]==2){
+                swap(nums[m]  , nums[h]) ; 
+                h-- ; 
+            }else if(nums[m]==1){
+               m++ ; 
+            }else{
+                swap(nums[m] , nums[l]) ; 
+                l++ ; 
+                m++ ; 
+            }
+        }
     }
 };
