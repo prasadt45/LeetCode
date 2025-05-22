9aaf8bbe5597893407ae753181288a875448a7d4
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-05-22 16:34:25 +0530

    [198] [Time Beats: 9.13%] [Memory Beats: 63.36%] - LeetPush

--- House-Robber.cpp
+++ House-Robber.cpp
@@ -1,23 +1,20 @@
 class Solution {
-    private:
-    int f(int n , vector<int>&nums , vector<int>&dp){
-        if(n==0){
-            return nums[0] ; 
-        }
-        if(n<0){
-            return 0  ; 
-        }
-        if(dp[n]!=-1){
-           return dp[n] ; 
-        }
-        int p = nums[n]+f(n-2 , nums , dp) ; 
-        int np = f(n-1 , nums , dp) ; 
-        return  dp[n] =  max(p , np) ; 
-    }
 public:
     int rob(vector<int>& nums) {
-        int n =nums.size() ; 
+         int n =nums.size() ; 
         vector<int>dp(n , -1) ; 
-        return f(n-1 , nums   , dp ) ; 
+        dp[0] = nums[0] ;
+        int ne = 0 ; 
+        for(int i = 1 ; i<n ; i++){
+            int p  = 0 ; 
+            if((i-2)<0){
+                p =  nums[i] ; 
+            }else{
+             p = nums[i] + dp[i-2]  ; 
+            }
+            int np = dp[i-1] ; 
+            dp[i] = max(p , np) ; 
+        }
+        return dp[n-1] ; 
     }
 };
