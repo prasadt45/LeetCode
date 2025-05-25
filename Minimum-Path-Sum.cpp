733d3bc04a21fd2e01cdb637fa44cbec79edf738
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-05-25 16:30:20 +0530

    [64] [Time Beats: 100.00%] [Memory Beats: 38.18%] - LeetPush

--- Minimum-Path-Sum.cpp
+++ Minimum-Path-Sum.cpp
@@ -1,26 +1,30 @@
 class Solution {
-    private:
-    int f(int i , int j  , vector<vector<int>>&grid , vector<vector<int>>&dp){
-        int mod = 1e9+7 ; 
-        if(i==0 && j==0){
-            return grid[0][0] ; 
-        }
-        if(i<0 || j<0){
-            return mod  ;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j] ; 
-        }
-        int lc =  f(i-1 , j ,  grid , dp) + grid[i][j]; 
-        int rc =     f(i , j-1 ,  grid , dp)+grid[i][j] ;
-       
-        return dp[i][j] =  min(lc ,rc) ; 
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size() ; 
         int m = grid[0].size() ; 
-        vector<vector<int>>dp(n  , vector<int>(m , -1)) ;
-        return f(n-1 , m-1, grid , dp) ; 
+        vector<vector<int>>dp(n  , vector<int>(m , 0)) ;
+        for(int i = 0 ;i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(i==0 && j ==0){
+                    dp[i][j] = grid[i][j] ; 
+                }else{
+                int lc = grid[i][j] ;
+                int rc = grid[i][j] ; 
+                if(i>0){
+                 lc = lc + dp[i-1][j] ; 
+                }else{
+                    lc+=1e9; 
+                }
+                if(j>0){
+                 rc = rc+ dp[i][j-1] ; 
+                }else{
+                    rc+=1e9;
+                }
+                dp[i][j] =  min(lc , rc) ; 
+            }
+            }
+        } 
+        return dp[n-1][m-1] ; 
     }
 };
