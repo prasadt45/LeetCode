4674f1186826cdc56231796eda138fb6af3c7b7e
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-03 15:57:40 +0530

    [2578] [Time Beats: 100.00%] [Memory Beats: 94.92%] - LeetPush

---
+++ Split-With-Minimum-Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int splitNum(int num) {
+        string s = to_string(num) ; 
+        sort(s.begin() , s.end()) ; 
+        string s1 ; 
+        string s2; 
+        for(int i = 0 ;i<s.size() ; i++){
+            if(i%2==0){
+                s1+=s[i] ; 
+            }else{
+                s2+=s[i] ; 
+            }
+        }
+        return stoi(s1)+stoi(s2) ; 
+
+    }
+};
