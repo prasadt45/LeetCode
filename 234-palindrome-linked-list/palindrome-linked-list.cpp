84e79a95fd913c33a6b126f1a09fc58743e0c4bf
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-07 16:55:28 +0530

    Time: 4 ms (53.38%) | Memory: 121.8 MB (50.02%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,24 +9,42 @@
  * };
  */
 class Solution {
+    private:
+    ListNode* rll(ListNode* head){
+       
+        ListNode* prev = NULL ;
+        ListNode* temp = head ; 
+        while(temp!=NULL){
+            ListNode* nn = temp->next ; 
+            temp->next = prev ; 
+            prev = temp ; 
+            temp = nn ; 
+        }
+        return prev ; 
+    }
 public:
     bool isPalindrome(ListNode* head) {
-        stack<int>st ; 
-        ListNode* temp = head  ; 
-        while(temp){
-            st.push(temp->val) ; 
-            temp=temp->next ;
-
+        if(head==NULL || head->next==NULL){
+            return true ; 
         }
-        temp=head ;
-        while(temp){
-            if(temp->val == st.top()){
-                st.pop() ; 
-                temp=temp->next ; 
-            }else{
+        ListNode* fast = head ; 
+        ListNode* slow = head ; 
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next ; 
+            fast = fast->next->next ; 
+        }
+        ListNode* newh = (rll(slow->next));
+        ListNode* h = head ; 
+        ListNode* nh = newh ; 
+        while(nh!=NULL){
+            if(h->val!=nh->val) {
+                rll(newh) ;
                 return false ; 
             }
+            h= h->next ; 
+            nh = nh->next ; 
         }
+        rll(newh) ;
         return true ; 
     }
 };
