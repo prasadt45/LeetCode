7cb602c5b2d83cc07aa5b5a4f01b96de58214296
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-07 12:40:28 +0530

    [518] [Time Beats: 38.57%] [Memory Beats: 17.85%] - LeetPush

--- Coin-Change-II.cpp
+++ Coin-Change-II.cpp
@@ -1,31 +1,29 @@
 class Solution {
-    private:
-    int f(int i , vector<int>&arr , int target , vector<vector<int>>&dp){
-        if(i==0){
-            int dif = target % arr[0] ; 
-            if(target % arr[0] ==0 ){
-                return 1   ; 
-            }else{
-                return 0 ; 
-            }
-        }
-        if(dp[i][target] != -1){
-           return dp[i][target] ; 
-        }
-
-        int nt = f(i-1 , arr , target , dp) ; 
-        int t = 0 ; 
-        if(arr[i]<=target){
-            t = f(i , arr ,  target  - arr[i] , dp) ; 
-        }
-        return  dp[i][target] =  t+nt ; 
-    }
 public:
     int change(int target, vector<int>& arr) {
-        int n = arr.size() ; 
-        vector<vector<int>>dp(n , vector<int>(target+1 , -1)) ; 
-        return f(n-1 , arr , target , dp) ; 
+        int n = arr.size();
+        vector<vector<unsigned long long>> dp(n, vector<unsigned long long>(target + 1, 0));
+        for (int t = 0; t <= target; t++) {
+            if (t % arr[0] == 0) {
+                dp[0][t] = 1ULL;
+            } else {
+                dp[0][t] = 0ULL;
+            }
+        }
 
-        
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= target; t++) {
+                unsigned long long notTake = dp[i - 1][t];
+                unsigned long long take = 0ULL;
+                if (arr[i] <= t) {
+                    take = dp[i][t - arr[i]];
+                }
+
+                unsigned long long sum = notTake + take;
+                dp[i][t] = (sum > INT_MAX) ? INT_MAX : sum;
+            }
+        }
+
+        return static_cast<int>(dp[n - 1][target]);
     }
 };
