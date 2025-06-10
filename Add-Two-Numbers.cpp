82716c4c6166efc13898ac84861b0dba0720952f
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-10 15:57:58 +0530

    [2] [Time Beats: 7.94%] [Memory Beats: 91.36%] - LeetPush

--- Add-Two-Numbers.cpp
+++ Add-Two-Numbers.cpp
@@ -11,26 +11,35 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dn = new ListNode(-1) ; 
-        ListNode* curr = dn ; 
         ListNode* t1 = l1 ;
         ListNode* t2 = l2 ; 
-        int cry = 0 ; 
-        while(t1!=NULL ||t2!=NULL){
-            int sum = cry ; 
-            if(t1) sum+=t1->val ; 
-            if(t2) sum+=t2->val ; 
-            ListNode* NN = new ListNode(sum%10) ; 
-            cry= sum/10 ; 
-            curr->next = NN ;
-            curr=curr->next ; 
-            if(t1) t1=t1->next ;
-            if(t2) t2=t2->next ; 
-
+        ListNode* dn = new ListNode(-1) ; 
+        ListNode* c = dn  ;
+        int cr = 0 ;
+        while(t1!=NULL || t2!=NULL){
+            int sum = cr ; 
+            if(t1){
+                sum+=t1->val ;
+            }
+            if(t2){
+                sum+=t2->val ; 
+            }
+            ListNode* nn = new ListNode(sum%10)  ;
+            cr = sum/10 ; 
+            c->next = nn ; 
+            c = nn ; 
+            if(t1){
+                t1=t1->next ; 
+            }
+            if(t2){
+                t2=t2->next ; 
+            }
         }
-        if(cry){
-            ListNode* NN = new ListNode(cry) ; 
-            curr->next = NN ; 
+        if(cr){
+            ListNode* newn = new ListNode(cr) ; 
+            c->next = newn ; 
+            c = newn ; 
+            c->next=NULL ; 
         }
         return dn->next ; 
     }
