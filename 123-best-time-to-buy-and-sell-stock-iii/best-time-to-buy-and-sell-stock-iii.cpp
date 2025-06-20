f7cd26af18c5eac6b384bd91f4cf4c96eaea5d70
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-20 17:02:11 +0530

    Time: 405 ms (41.62%) | Memory: 211.6 MB (38.78%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,38 @@
 class Solution {
-    private:
-    int f(int i , int b , int c , vector<int>&prices , vector<vector<vector<int>>>&dp){
-        int n = prices.size() ; 
-        if(i==n) return 0 ; 
-        if(c==0) return 0 ; 
-        if(dp[i][b][c]!=-1){
-            return dp[i][b][c] ; 
-        }
-        if(b==1){
-            return dp[i][b][c] =  max(
-                -prices[i] + f(i+1 , 0 , c , prices , dp) , 
-                 0 + f(i+1 , 1 , c , prices , dp)
-            ) ; 
-        }else{
-             return dp[i][b][c] =  max(
-                prices[i] + f(i+1 , 1 , c-1 , prices , dp) , 
-                 0 + f(i+1 , 0 , c , prices , dp)
-            ) ; 
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size() ; 
-        vector<vector<vector<int>>>dp(n , vector<vector<int>>(2 , vector<int>(3 , -1))) ; 
-        return f(0 , 1 , 2 , prices , dp) ; 
+        // think of tabulation ;
+        // base case ? 
+        vector<vector<vector<int>>>dp(n+1 , vector<vector<int>>(2 , vector<int>(3 , -1))) ; 
+        for(int i = 0 ; i<=1 ; i++){
+            for(int j = 0 ; j<=2 ; j++){
+                dp[n][i][j] = 0 ; 
+            }
+        }
+        for(int i = 0 ; i<=n ; i++){
+            for(int j = 0 ; j<=1 ; j++){
+                dp[i][j][0] = 0 ; 
+            }
+        }
+        
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int j = 0 ; j<=1 ;j++){
+                for(int c = 1 ; c<=2 ; c++){
+                    if(j==1){
+                        dp[i][j][c] = max(
+                            -prices[i] + dp[i+1][0][c] , 
+                            0 + dp[i+1][1][c] 
+                        ) ; 
+                    }else{
+                        dp[i][j][c] = max(
+                            prices[i] + dp[i+1][1][c-1] , 
+                            0+dp[i+1][0][c] 
+                        ); 
+                    }
+                }
+            }
+        }
+        return dp[0][1][2] ; 
     }
 };
