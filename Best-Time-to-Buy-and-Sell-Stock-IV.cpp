d8572c5c40e26c2f93d2dfcc76249bf3b3fce2ab
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-20 17:40:18 +0530

    [188] [Time Beats: 62.04%] [Memory Beats: 36.10%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-IV.cpp
+++ Best-Time-to-Buy-and-Sell-Stock-IV.cpp
@@ -1,29 +1,36 @@
 class Solution {
-  private:
-    int f(int i , int b , int c , vector<int>&prices , vector<vector<vector<int>>>&dp){
-        int n = prices.size() ; 
-        if(i==n) return 0 ; 
-        if(c==0) return 0 ; 
-        if(dp[i][b][c]!=-1){
-            return dp[i][b][c] ; 
-        }
-        if(b==1){
-            return dp[i][b][c] =  max(
-                -prices[i] + f(i+1 , 0 , c , prices , dp) , 
-                 0 + f(i+1 , 1 , c , prices , dp)
-            ) ; 
-        }else{
-             return dp[i][b][c] =  max(
-                prices[i] + f(i+1 , 1 , c-1 , prices , dp) , 
-                 0 + f(i+1 , 0 , c , prices , dp)
-            ) ; 
-        }
-    }
-
 public:
     int maxProfit(int k, vector<int>& prices) {
-       int n = prices.size() ; 
-        vector<vector<vector<int>>>dp(n , vector<vector<int>>(2 , vector<int>(k+1 , -1))) ; 
-        return f(0 , 1 , k , prices , dp) ;  
+        int n = prices.size() ; 
+        vector<vector<vector<int>>>dp(n+1 , vector<vector<int>>(2 , vector<int>(k+1 , 0))) ; 
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<=1 ; j++){
+                dp[i][j][0] = 0 ; 
+            }
+        }
+        for(int i = 0 ; i<=1 ; i++){
+            for(int j = 0 ; j<=k ; j++){
+               dp[n][i][j] = 0 ; 
+            }
+       }
+
+       for(int i = n-1 ; i>=0 ; i--){
+        for(int j = 0 ; j<=1 ; j++){
+            for(int c = 1 ; c<=k ; c++){
+                if(j==1){
+                        dp[i][j][c] = max(
+                            -prices[i] + dp[i+1][0][c] , 
+                            0 + dp[i+1][1][c] 
+                        ) ; 
+                    }else{
+                        dp[i][j][c] = max(
+                            prices[i] + dp[i+1][1][c-1] , 
+                            0+dp[i+1][0][c] 
+                        ); 
+                    }
+            }
+        }
+       }
+       return dp[0][1][k] ; 
     }
 };
