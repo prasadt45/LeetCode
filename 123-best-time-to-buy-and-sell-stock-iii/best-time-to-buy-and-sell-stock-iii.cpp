03f12be02289a72f44e338da9e204f90d5b9e748
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-20 17:16:59 +0530

    Time: 56 ms (67.53%) | Memory: 79.2 MB (96.47%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,35 +4,29 @@ public:
         int n = prices.size() ; 
         // think of tabulation ;
         // base case ? 
-        vector<vector<vector<int>>>dp(n+1 , vector<vector<int>>(2 , vector<int>(3 , -1))) ; 
-        for(int i = 0 ; i<=1 ; i++){
-            for(int j = 0 ; j<=2 ; j++){
-                dp[n][i][j] = 0 ; 
-            }
-        }
-        for(int i = 0 ; i<=n ; i++){
-            for(int j = 0 ; j<=1 ; j++){
-                dp[i][j][0] = 0 ; 
-            }
-        }
+      vector<vector<int>>after(2 , vector<int>(3 , 0)) ; 
+      vector<vector<int>>curr(2 , vector<int>(3 , 0)) ; 
+    
         
         for(int i = n-1 ; i>=0 ; i--){
             for(int j = 0 ; j<=1 ;j++){
                 for(int c = 1 ; c<=2 ; c++){
                     if(j==1){
-                        dp[i][j][c] = max(
-                            -prices[i] + dp[i+1][0][c] , 
-                            0 + dp[i+1][1][c] 
+                        curr[j][c] = max(
+                            -prices[i] + after[0][c] , 
+                            0 + after[1][c] 
                         ) ; 
                     }else{
-                        dp[i][j][c] = max(
-                            prices[i] + dp[i+1][1][c-1] , 
-                            0+dp[i+1][0][c] 
+                        curr[j][c] = max(
+                            prices[i] +after[1][c-1] , 
+                            0+after[0][c] 
                         ); 
                     }
                 }
             }
+            after =  curr ; 
         }
-        return dp[0][1][2] ; 
+        return after[1][2] ; 
+
     }
 };
