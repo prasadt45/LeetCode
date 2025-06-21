cdcb6286f5141da3b8be1183e9d92153e8ab9223
Prasad Tamhan <146625931+prasadt45@users.noreply.github.com>
2025-06-21 19:32:10 +0530

    Time: 99 ms (45.15%) | Memory: 95 MB (53.08%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,29 +1,46 @@
 class Solution {
-     private:
-    int f(int i , int b , vector<int>&prices , vector<vector<int>>&dp , int fee){
-        int n = prices.size() ; 
-        if(i==n){
-            return 0 ; 
-        }
-        if(dp[i][b]!=-1){
-            return dp[i][b] ; 
-        }
-        if(b==1){
-            return dp[i][b] =  max(
-                -prices[i]+f(i+1 , 0 , prices , dp , fee)  , 
-                0+f(i+1 , 1 , prices , dp ,fee) 
-            ) ; 
-        }else{
-            return dp[i][b] =  max(
-               ( prices[i]-fee ) + f(i+1 , 1 , prices , dp , fee ) , 
-                0+f(i+1 , 0 , prices , dp , fee) 
-            );
-        }
-    }
+    //  private:
+    // int f(int i , int b , vector<int>&prices , vector<vector<int>>&dp , int fee){
+    //     int n = prices.size() ; 
+    //     if(i==n){
+    //         return 0 ; 
+    //     }
+    //     if(dp[i][b]!=-1){
+    //         return dp[i][b] ; 
+    //     }
+    //     if(b==1){
+    //         return dp[i][b] =  max(
+    //             -prices[i]+f(i+1 , 0 , prices , dp , fee)  , 
+    //             0+f(i+1 , 1 , prices , dp ,fee) 
+    //         ) ; 
+    //     }else{
+    //         return dp[i][b] =  max(
+    //            ( prices[i]-fee ) + f(i+1 , 1 , prices , dp , fee ) , 
+    //             0+f(i+1 , 0 , prices , dp , fee) 
+    //         );
+    //     }
+    // }
 public:
     int maxProfit(vector<int>& prices, int fee) {
          int n = prices.size() ; 
-        vector<vector<int>>dp(n , vector<int>(2 , -1)) ; 
-        return f(0 , 1 , prices , dp , fee) ; 
+        vector<vector<int>>dp(n+1 , vector<int>(2 , -1)) ; 
+        dp[n][0] = 0 ; 
+        dp[n][1] = 0 ; 
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int b = 0 ; b<=1 ; b++){
+                if(b==1){
+                    dp[i][b] = max(
+                         -prices[i]+dp[i+1][0]  , 
+                          0+dp[i+1][1]
+                    );
+                }else{
+                    dp[i][b] =  max(
+                   ( prices[i]-fee ) + dp[i+1][1] , 
+                   0+dp[i+1][0]
+            );
+                }
+            }
+        }
+        return dp[0][1] ; 
     }
 };
